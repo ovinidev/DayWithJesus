89bd1e898b1fb4f680e208e668d5eba3a98db779
ovinidev <vinicius.celestino@pagar.me>
2026-04-02 10:27:17 -0300

    flags integration

--- src/composables/useFeatureFlagActive.ts
+++ src/composables/useFeatureFlagActive.ts
@@ -4,7 +4,9 @@ import { useFlags } from 'flagsmith-vue'
 export const useFeatureFlagActive = (flag: string) => {
   const flags = useFlags([flag])
 
-  const isFeatureFlagActive = computed(() => flags[flag].value?.value === true)
+  const isFeatureFlagActive = computed(
+    () => flags[flag].value?.enabled === true
+  )
 
   return { isFeatureFlagActive }
 }
