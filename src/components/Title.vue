41e7a98b7f9ad9d4642a490832ea442ec677602f
ovinidev <vinicius.celestino@pagar.me>
2026-04-02 15:30:56 -0300

    chore: add link

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -2,11 +2,18 @@
   const { title } = defineProps<{
     title: string
   }>()
+
+  const openYoutubeSearch = () => {
+    window.open(
+      'https://www.youtube.com/results?search_query=padre+adriano+zandon%C3%A1&sp=EgQIAhAB'
+    )
+  }
 </script>
 
 <template>
   <h1
     class="mt-8 text-center text-3xl font-semibold tracking-wide text-black md:text-3xl"
+    @click="openYoutubeSearch"
   >
     {{ title }}
   </h1>
